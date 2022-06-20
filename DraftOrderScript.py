9618ad8230eda5cf5151dc69545c530f3f668176
leeaj9719 <leeaj9719@gmail.com>
2022-06-19 20:25:18 -0500

    add draft order script

---
+++ DraftOrderScript.py
@@ -0,0 +1,14 @@
+import random
+import time
+
+OWNERS = ["Andrew", "Craig", "Dalton", "Jake", "Justin", "Matt", "Mike", "Paul", "Selim", "Taylor"]
+WINNER = "Selim"
+WINNERS_CHOICE = 3
+
+OWNERS.remove(WINNER)
+random.shuffle(OWNERS)
+OWNERS.insert(WINNERS_CHOICE-1, WINNER)
+
+for i in range(0, len(OWNERS)):
+    time.sleep(1) # Add dramatic pauses
+    print(f"{i+1}.) {OWNERS[i]}")
