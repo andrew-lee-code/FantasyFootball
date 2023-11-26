b5abf401798b1b7837535062ea84d8f021432c50
andrew632 <andrew@demicomposting.com>
2023-11-26 09:27:54 -0500

    added poopoo comment

--- DraftOrderScript.py
+++ DraftOrderScript.py
@@ -7,8 +7,10 @@ WINNERS_CHOICE = 3
 
 OWNERS.remove(WINNER)
 random.shuffle(OWNERS)
-OWNERS.insert(WINNERS_CHOICE-1, WINNER)
+OWNERS.insert(WINNERS_CHOICE - 1, WINNER)
 
 for i in range(0, len(OWNERS)):
-    time.sleep(1) # Add dramatic pauses
+    time.sleep(1)  # Add dramatic pauses
     print(f"{i+1}.) {OWNERS[i]}")
+
+# poopoo
